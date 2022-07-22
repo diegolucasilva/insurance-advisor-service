5edb25fbcc5a9f0e0110d62ff6224bad58f7a2c7
Diego Lucas <diego.lucasilva@gmail.com>
2022-07-22 19:39:20 -0300

    chore: inicial commit

---
+++ src/main/kotlin/com/dls/insuranceadvisorservice/InsuranceAdvisorServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.dls.insuranceadvisorservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InsuranceAdvisorServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<InsuranceAdvisorServiceApplication>(*args)
+}
