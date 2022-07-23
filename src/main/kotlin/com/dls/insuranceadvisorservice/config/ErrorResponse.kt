126062b3859174393f1232d92279948abe00ef65
diegolucasilva <diego.lucasilva@gmail.com>
2022-07-23 16:15:35 -0300

    feat: Handle exceptions

---
+++ src/main/kotlin/com/dls/insuranceadvisorservice/config/ErrorResponse.kt
@@ -0,0 +1,18 @@
+package com.dls.insuranceadvisorservice.config
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
+data class ErrorResponse(val errorMessages: List<ErrorDescriptor>) {
+    constructor(parameter_name: String? = null, description: String? = null) :
+            this(listOf(ErrorDescriptor(parameter_name, description)))
+
+    data class ErrorDescriptor(
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        val parameterName: String?=null,
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        val description: String?=null
+    )
+}
